65370d7af7a49d23711d24bb12f3bcfa9aeea7a2
Pedro Neto <pedroneto@Pedros-MacBook-Air.local>
2019-01-23 15:18:30 -0300

    finish FormLogin component

--- App/Components/Common/FormLogin.js
+++ App/Components/Common/FormLogin.js
@@ -1,21 +1,23 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
 
+//styles
+import styles from './Styles/FormLoginStyle';
 
 export default class App extends Component {
   render() {
     return (
-      <View> 
-        <View>
-            <Text> App de Mensagem</Text>
+      <View style={styles.container}> 
+        <View style={styles.containerHeader}>
+            <Text style={styles.titleHeader}> App de Mensagem</Text>
         </View>
-        <View>
-            <TextInput placeholder="Email" />
-            <TextInput placeholder="Senha" />
-            <Text>Ainda não tem cadastro? Cadastre-se</Text>
+        <View style={styles.containerInput}>
+            <TextInput style={styles.textInput} placeholder="Email" />
+            <TextInput style={styles.textInput} placeholder="Senha" />
+            <Text style={styles.textSignUp}>Ainda não tem cadastro? Cadastre-se</Text>
         </View>
-        <View>
-            <Button title="Acessar" onPress={() => false} />
+        <View style={styles.containerButton}>
+            <Button title="Acessar" color="#115E54" onPress={() => false} />
         </View>
       </View>
     );
---
+++ App/Components/Common/Styles/FormLoginStyle.js
@@ -0,0 +1,29 @@
+import { StyleSheet, Platform } from 'react-native';
+
+export default StyleSheet.create({
+	container: {
+        flex: 1,
+        padding: 10
+    },
+    containerHeader: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    containerInput: {
+        flex: 2
+    },
+    containerButton: {
+        flex: 2
+    },
+    titleHeader: {
+        fontSize: 25
+    },
+    textInput: {
+        fontSize: 20,
+        height: 45
+    },
+    textSignUp: {
+        fontSize: 20
+    }
+})
