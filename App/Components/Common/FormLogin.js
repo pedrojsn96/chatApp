b816e4ada70d7eaf2d4959256ad8780abc7bf57e
Pedro Neto <pedroneto@Pedros-MacBook-Air.local>
2019-01-24 20:37:01 -0300

    adding background image

--- App/Components/Common/FormLogin.js
+++ App/Components/Common/FormLogin.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, Button, TextInput, TouchableHighlight} from 'react-native';
+import {StyleSheet, Text, View, Button, TextInput, TouchableHighlight, ImageBackground} from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { connect } from 'react-redux';
 import { setEmail, setPassword } from '../../Containers/Redux/Actions/AuthActions'; 
@@ -10,21 +10,23 @@ import styles from './Styles/FormLoginStyle';
 class FormLogin extends Component {
   render() {
     return (
-      <View style={styles.container}> 
-        <View style={styles.containerHeader}>
-            <Text style={styles.titleHeader}> App de Mensagem</Text>
+      <ImageBackground source={require('../../Images/bg.png')} style={{ flex: 1, width: null }}>
+        <View style={styles.container}> 
+          <View style={styles.containerHeader}>
+              <Text style={styles.titleHeader}> App de Mensagem</Text>
+          </View>
+          <View style={styles.containerInput}>
+              <TextInput value={this.props.email} style={styles.textInput} placeholder="Email" onChangeText={(text) => this.props.setEmail(text)} />
+              <TextInput secureTextEntry value={this.props.password} style={styles.textInput} placeholder="Senha" onChangeText={(password) => this.props.setPassword(password)} />
+              <TouchableHighlight onPress={ () => Actions.formSignUp() }>
+                <Text style={styles.textSignUp}>Ainda não tem cadastro? Cadastre-se</Text>
+              </TouchableHighlight>
+          </View>
+          <View style={styles.containerButton}>
+              <Button title="Acessar" color="#115E54" onPress={() => false} />
+          </View>
         </View>
-        <View style={styles.containerInput}>
-            <TextInput value={this.props.email} style={styles.textInput} placeholder="Email" onChangeText={(text) => this.props.setEmail(text)} />
-            <TextInput secureTextEntry value={this.props.password} style={styles.textInput} placeholder="Senha" onChangeText={(password) => this.props.setPassword(password)} />
-            <TouchableHighlight onPress={ () => Actions.formSignUp() }>
-              <Text style={styles.textSignUp}>Ainda não tem cadastro? Cadastre-se</Text>
-            </TouchableHighlight>
-        </View>
-        <View style={styles.containerButton}>
-            <Button title="Acessar" color="#115E54" onPress={() => false} />
-        </View>
-      </View>
+      </ImageBackground>
     );
   }
 }
--- App/Components/Common/FormSignUp.js
+++ App/Components/Common/FormSignUp.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import {StyleSheet, Text, View, Button, TextInput, ImageBackground} from 'react-native';
 import { connect } from 'react-redux';
 import { setEmail, setPassword, setName } from '../../Containers/Redux/Actions/AuthActions'; 
 //styles
@@ -8,16 +8,18 @@ import styles from './Styles/FormLoginStyle';
 class FormSignUp extends Component {
   render() {
     return (
-      <View style={styles.container}> 
-        <View style={styles.containerInput}>
-            <TextInput value={this.props.name} style={styles.textInput} placeholder="Nome" onChangeText={(name) => this.props.setName(name)} />
-            <TextInput value={this.props.email} style={styles.textInput} placeholder="Email" onChangeText={(text) => this.props.setEmail(text)} />
-            <TextInput secureTextEntry value={this.props.password} style={styles.textInput} placeholder="Senha" onChangeText={(password) => this.props.setEmail(password)} />
+      <ImageBackground source={require('../../Images/bg.png')} style={{ flex: 1, width: null }}>
+        <View style={styles.container}> 
+          <View style={styles.containerInput}>
+              <TextInput value={this.props.name} style={styles.textInput} placeholder="Nome" onChangeText={(name) => this.props.setName(name)} />
+              <TextInput value={this.props.email} style={styles.textInput} placeholder="Email" onChangeText={(text) => this.props.setEmail(text)} />
+              <TextInput secureTextEntry value={this.props.password} style={styles.textInput} placeholder="Senha" onChangeText={(password) => this.props.setEmail(password)} />
+          </View>
+          <View style={styles.containerButton}>
+              <Button title="Cadastrar" color="#115E54" onPress={() => false} />
+          </View>
         </View>
-        <View style={styles.containerButton}>
-            <Button title="Cadastrar" color="#115E54" onPress={() => false} />
-        </View>
-      </View>
+      </ImageBackground>
     );
   }
 }
