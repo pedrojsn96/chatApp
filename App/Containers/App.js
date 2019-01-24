d00646aab71e5f4be730533e0c04f1bafc58f306
Pedro Neto <pedroneto@Pedros-MacBook-Air.local>
2019-01-24 20:59:34 -0300

    adding firebase

--- App/Containers/App.js
+++ App/Containers/App.js
@@ -8,8 +8,29 @@ import { createStore } from 'redux';
 
 import Routes from './Routes';
 import reducers from './Redux/Reducers';
+// import firebase from 'firebase'; dar erro no android
+import firebase from '@firebase/app';
+import '@firebase/auth';
+import '@firebase/database';
 
 export default class App extends Component {
+
+  componentDidMount(){
+    this._onInitializeApp();
+  }
+
+  _onInitializeApp = () => {
+		// Initialize Firebase
+		return firebase.initializeApp({
+			apiKey: "AIzaSyBvDCVOhXPq8eWDHvaj2Iw6TiYJT0PVvDA",
+      authDomain: "app-mensagem-6b000.firebaseapp.com",
+      databaseURL: "https://app-mensagem-6b000.firebaseio.com",
+      projectId: "app-mensagem-6b000",
+      storageBucket: "app-mensagem-6b000.appspot.com",
+      messagingSenderId: "282147069718"
+		});
+	};
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
